7dfe9560eba62290ac40183102a1a1944c6442ca
Ynoa Pedro <ynoa.pedro@outlook.com>
2019-07-24 23:04:00 -0300

    Creating async timer hosted service

---
+++ Hello_Service/Hello_Service/Services/HelloWorldHostedService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hello_Service.Services
+{
+    public class HelloWorldHostedService : BackgroundService
+    {
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Debug.WriteLine("Hello World");
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+    }
+}
--- Hello_Service/Hello_Service/Startup.cs
+++ Hello_Service/Hello_Service/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hello_Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -21,19 +22,12 @@ namespace Hello_Service
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHostedService<HelloWorldHostedService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
